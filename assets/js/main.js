fb82cd47589a9dd50fe179369ecf09f243fbcd8c
HoangTon-99 <ducton1012@gmail.com>
2021-11-24 14:53:57 +0700

    upload trangchu new Signed-off-by: HoangTon-99 <ducton1012@gmail.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,20 @@
-
 $(document).ready(function () {
-    $('#toggle').click(function () {
-        $('menu').slideToggle();
-    });
-    
     $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        nav:true,
-        responsive:{
-            0:{
-                items:2
+        loop: true,
+        margin: 10,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 1000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 2
             },
-            768:{
-                items:3
+            768: {
+                items: 3
             },
-            992:{
-                items:4
+            992: {
+                items: 4
             }
         }
     })
