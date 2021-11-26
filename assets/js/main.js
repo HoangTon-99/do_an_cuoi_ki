4b802b13f720ea4f249b36a945ac81113a66b227
HoangTon-99 <ducton1012@gmail.com>
2021-11-26 11:07:50 +0700

    commit new Signed-off-by: HoangTon-99 <ducton1012@gmail.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,30 @@
+const body = document.querySelector("body" );
+const navbar = document.querySelector(".navbar__header" );
+const menu = document.querySelector(".menu-list");
+const menuBtn = document.querySelector(".menu-btn");
+const cannelBtn = document.querySelector(".cannel-btn");
+menuBtn.onclick = () => {
+    menu.classList.add("active")
+    menuBtn.classList.add("hide")
+    body.classList.add("disabledScroll")
+}
+cannelBtn.onclick = () => {
+    menu.classList.remove("active")
+    menuBtn.classList.remove("hide")
+    body.classList.remove("disabledScroll")
+
+}
+
+window.onscroll = () => {
+    this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky")
+}
 $(document).ready(function () {
     $('.owl-carousel').owlCarousel({
         loop: true,
         margin: 10,
         nav: true,
         autoplay: true,
-        autoplayTimeout: 1000,
+        autoplayTimeout: 2000,
         autoplayHoverPause: true,
         responsive: {
             0: {
