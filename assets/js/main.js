49e835bffea860664a0314f28f078d5ddc6c37d0
HoangTon-99 <ducton1012@gmail.com>
2021-12-06 14:59:03 +0700

    trang chu hoan thanh Signed-off-by: HoangTon-99 <ducton1012@gmail.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,19 @@
-const body = document.querySelector("body" );
-const navbar = document.querySelector(".navbar__header" );
+// AOS
+AOS.init();
+
+// Tooltip
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+const body = document.querySelector("body");
+const navbar = document.querySelector(".navbar__header");
 const menu = document.querySelector(".menu-list");
 const menuBtn = document.querySelector(".menu-btn");
 const cannelBtn = document.querySelector(".cannel-btn");
+const menuHr = document.querySelector(".menu__hr");
+
 menuBtn.onclick = () => {
     menu.classList.add("active")
     menuBtn.classList.add("hide")
@@ -17,10 +28,26 @@ cannelBtn.onclick = () => {
 
 window.onscroll = () => {
     this.scrollY > 20 ? navbar.classList.add("sticky") : navbar.classList.remove("sticky")
+    this.scrollY > 20 ? menuHr.classList.add("hide") : menuHr.classList.remove("hide")
 }
+
 $(document).ready(function () {
+    //back button
+    $('.btn-back-to-top').hide();
+			$(window).scroll(function () {
+				if ($(this).scrollTop() > 200) {
+					$('.btn-back-to-top').fadeIn(500);
+				} else {
+					$('.btn-back-to-top').fadeOut(500);
+				}
+			});
+			$('.btn-back-to-top').click(function () {
+				$("html").animate({
+					scrollTop: 0
+				},0);
+			});
     $('.owl-carousel').owlCarousel({
-        loop: true,
+        // loop: true,
         margin: 10,
         nav: true,
         autoplay: true,
@@ -39,3 +66,4 @@ $(document).ready(function () {
         }
     })
 })
+
